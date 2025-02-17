fa9155e1e2b0affd3e66abbda8df2e51eed335fc
brunojoao <brunokataklysm@gmail.com.br>
2025-02-17 13:52:41 -0300

    no swapfile

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.autoindent = true
+vim.opt.swapfile = false
 vim.filetype.add({
   pattern = {
     ['.*%.blade%.php'] = 'blade',

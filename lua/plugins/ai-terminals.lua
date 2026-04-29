b683ca570558288e14067a33283edcd4354686d0
brunojoao <brunokataklysm@gmail.com.br>
2026-04-29 12:39:06 -0300

    ai-terminals

--- lua/plugins/ai-terminals.lua
+++ lua/plugins/ai-terminals.lua
@@ -13,7 +13,7 @@ return {
         },
         aider = {
           cmd = function()
-            return "aider --model gemini/gemini-2.5-flash-latest --watch-files"
+            return "aider --model gemini/gemini-2.5-flash --watch-files"
           end,
           path_header_template = "`%s`",
         },
